56e2922bd18e0e71965c06d1a80a72be492703b3
Ron Jonk <avrjonk@svb.nl>
2019-09-14 02:19:32 +0200

    added images

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -32,7 +32,11 @@ module.exports = merge(common, {
       },
       {
         test: /\.s?css$/i,
-        use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader']
+        use: [
+          'style-loader',
+          'css-loader?sourceMap=true',
+          'sass-loader'
+        ]
       }
     ]
   },
